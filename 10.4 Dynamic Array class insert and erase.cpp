203e3be961eb85bc251b2611ad721ddaf29383c5
Aina Mestres <aina.mestres@estudiantat.upc.edu>
2024-12-04 01:05:40 +0100

    Preguntar profe

--- 10.4 Dynamic Array class insert and erase.cpp
+++ 10.4 Dynamic Array class insert and erase.cpp
@@ -1,9 +1,29 @@
 // 10.4 Dynamic Array class insert and erase.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+//Crear funciones para borrar un elemento e insertar
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
+class vectores
+{
+public:
+    int a [3] = {1, 2, 3};
+
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+    vectores v, w;
+    //Con array
+    int v[3] = { 1, 2, 3 };
+
+    //Borrar ultimo elemento
+    for ( auto i = 0; i< v.size(); i++)
+    
+    //Con vectores
+    vector <int> w = { 1, 2, 3 };
+    for (int i = 0; i < w.size(); i++)
+    {
+
+    }
 }
